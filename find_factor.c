52811f7db8d298922fedd0eac163a9ac5fd6da7b
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-13 20:50:07 +0000

    hot fix

--- find_factor.c
+++ find_factor.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long int* find_factor(unsigned long long int n);
-
-
-long long int* find_factor(unsigned long long int n)
+long int* find_factor(long long int n)
 {
 	long long int m = n / 2;
-	long long int *p;
-	long long int j = 0;
+	long int *p;
+	int j = 0;
 	int b = 2;
 	p = malloc(b * sizeof(long long int));
 	for (long long int i = 2; i <= m; i++)
@@ -27,7 +24,7 @@ long long int* find_factor(unsigned long long int n)
 }
 
 
-long long int main(void)
+long int main(void)
 {
 	//int arr[];
 	/*int *p;
@@ -39,14 +36,14 @@ long long int main(void)
 		*(p + i) = i;
 		n++;
 	}*/
-	unsigned long long int n = 1789664206425587715;
-	long long int *p;
+	long long int n = 1789664206425587715;
+	long int *p;
        	//p = malloc(n * sizeof(long long int));
 	p = find_factor(n);
 
-	for (long long int j = 0; *(p + j) != '\0'; j++)
+	for (int j = 0; *(p + j) != '\0'; j++)
 	{
-		printf("%lld ", *(p + j));
+		printf("%ld ", *(p + j));
 	}
 	printf("\n");
 }
