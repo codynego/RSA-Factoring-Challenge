69e5ab3c4f2b31e2aa284f9c4ac6214f661e22c3
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-15 13:03:51 +0000

    hot fix

--- file.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-
-long int* find_factor(long long int n);
-int *find_prime(int n);
-#endif
--- main.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-from unix_time import unix_time
-from factors import main
